450b1417ce2056c1f12282327a61eced54eb6472
Bebrohub12 <sudhirmishra5738@gmail.com>
2024-11-10 15:55:20 +0530

    password genarator prectice done

--- 04project/passwordGenretor/src/App.jsx
+++ 04project/passwordGenretor/src/App.jsx
@@ -1,9 +1,31 @@
 import React, { useState } from 'react'
 
 function App() {
-  const [data,setData]=useState()
+
+  const [password, setPassword] = useState("xyz")
+  const [numAllow, setNumAllow] = useState(false)
+
+  const genratePass = () => {
+    let pass = ""
+    let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    if(numAllow){
+      str+="0123456789"
+    }
+    for (let i = 1; i <= 5; i++) {
+      let char = Math.floor(Math.random() * str.length)
+      pass += str[char]
+    }
+
+    setPassword(pass)
+  }
+
   return (
-    <div className='bg-green-600 text-center  '>App</div>
+    <div>
+      <h2>{password}</h2>
+      <input type="checkbox" value={numAllow} onClick={() => setNumAllow((prev) => !prev)} />
+      <label htmlFor="">Number</label>
+      <button onClick={genratePass}>genratePassword</button>
+    </div>
   )
 }
 
